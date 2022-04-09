7bfdf80caaa4217e96eceb9d8076fbfcab520902
JoshuaBeals <joshua@camelcase.co>
2022-04-09 13:03:24 +0100

    Wired up mongoDB and mongoose

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import './services/passport.js';
+import keys from "./config/keys.js";
 
+// Connect to DB
+mongoose.connect(keys.mongoURI)
+.then(() => console.log('DB Connection Successful!'))
+.catch((err) => console.log(err));
+
+// Initialize app
 const app = express();
 
-//Import Routes
+// Import Routes
 import authRoute from './routes/authRoutes.js';
 
-//Route Middlewares
+// Route Middlewares
 app.use('/auth/google', authRoute);
 
 const PORT = process.env.PORT || 5000;
