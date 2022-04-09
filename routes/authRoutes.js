1e8d5846c8e106750b60933466b9a16074f97fcc
JoshuaBeals <joshua@camelcase.co>
2022-04-09 11:48:05 +0100

    Refactored folder structure

---
+++ routes/authRoutes.js
@@ -0,0 +1,10 @@
+const passport = require('passport');
+const router = require('express').Router();
+
+router.get('/', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+router.get('/callback', passport.authenticate('google'));
+
+module.exports = router;
